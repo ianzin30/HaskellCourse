73d22ddcc62673d76fbc5435a21dad88ae9ea527
ianzin30 <leloco61@gmail.com>
2023-12-05 19:10:38 -0300

    Section 8 was already done too, my bad

---
+++ Section8.hs
@@ -0,0 +1,10 @@
+-- testar map
+por2 :: Int -> Int
+por2 x = x * 2
+
+
+-- ESTUDAR ANONYMOUS FUNCTIONS POR FORA!!
+
+-- Função anônima = função sem nome
+-- \x -> ... 
+-- map (\x -> x * 4) [1,4,9,3]
